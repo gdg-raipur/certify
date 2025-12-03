ca91c9a0487138a01cf2f163a70b41d6fd6385b2
codeIntrovert <9131411681hasan@gmail.com>
2025-12-03 08:01:22 +0530

    [CONFIG] adds config and setup files

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface CertificateData {
+    id: string;
+    name: string;
+    verifyLink?: string;
+    design?: string;
+    [key: string]: string | undefined;
+}
+
+export interface CsvColumnMapping {
+    name: string;
+    verifyLink: string;
+    design: string;
+}
